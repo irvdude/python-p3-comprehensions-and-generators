eb4c1d939a21a18d2319a704b6944385d6e0c4dc
Irvin Dinkins <irvdude44@gmail.com>
2023-07-15 20:00:34 -0400

    passed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
+    even_elements = [n for n in num_list if n % 2 == 0]
+    return even_elements
     pass
 
+
 def make_exclamation(sentence_list):
+    excalmation_list = [n + "!" for n in sentence_list]
+    return excalmation_list
     pass
